ded5bf5b984a66e036a633447d2b0e3c24489cc7
conrad96 <conrad.mugisha@gmail.com>
2022-10-04 08:43:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {gql, useQuery} from '@apollo/client'
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const FetchCharacters = () => {
+
+    const GET_CHARACTERS = gql`
+        query Query {
+            characters {
+                results {
+                    id
+                    name
+                    image
+                    location {
+                        name 
+                        dimension
+                    }
+                }
+              }
+        }
+    `;
+
+    const {data, loading, error} = useQuery(GET_CHARACTERS);
+
+    if(loading) return <p>Loading....</p>
+    if(error) return <p>Error occured.. {error} </p>
+
+    return data.characters.results.map(({id, name, image, location}) => {
+        return (
+        <div key={id} className="character_section">
+            <div className="character_image">
+                <img src={`${image}`} className="image" alt={`${name}`} />
+            </div>
+            <div className="character_name">{name}</div>
+            <div className="character_location">
+                <p>Location: {location.name}</p>
+                <p>Dimension: {location.dimension}</p>
+            </div>
+        </div>
+        )
+    });
+}
+
+const App = () => {
+    return (
+        <div className="body_section">
+            <FetchCharacters />
+        </div>
+    )
 }
 
 export default App;
